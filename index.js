49bff3d76a66d9e787bc2bb64743f7ba08ca6e9b
Paula Caldas <pcaldas1995@hotmail.com>
2024-09-14 14:01:38 -0300

    carregar metas e salvar metas

--- index.js
+++ index.js
@@ -1,13 +1,24 @@
 const { select, input, checkbox } = require('@inquirer/prompts')
+const fs = require("fs").promises
 
 let mensagem = "Bem-vindo ao app de metas!";
 
-let meta ={
-  value:'Tomar 3L de água por dia',
-  checked: false,
+let metas 
+
+const carregarMetas = async () => {
+  try {
+    const dados = await fs.readFile("metas.json", "utf-8")
+    metas = JSON.parse(dados)
+  }
+  catch(erro) {
+    metas = []
+  }
 }
 
-let metas = [ meta ]
+const salvarMetas = async () => {
+  await fs.writeFile("metas.json", JSON.stringify(metas, null, 2))
+}
+
 
 const cadastrarMeta = async () => {
     const meta = await input({ message: "Digite a meta:"})
@@ -130,9 +141,12 @@ const mostrarMensagem = () => {
 }
 
 const start = async () => {
+  await carregarMetas()
+  
 
   while(true){
     mostrarMensagem()
+    await salvarMetas()
 
     const opcao = await select({
         message: "Menu >",
@@ -171,7 +185,6 @@ const start = async () => {
         break
       case "listar":
         await listarMetas()
-
         break
       case "realizadas":
         await metasRealizadas()
